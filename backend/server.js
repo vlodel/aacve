bad4f8b0eebf16c83321490fea4574c4f5460144
Vlad Apostol <vladdapostol@gmail.com>
2020-10-29 14:13:04 +0200

    added a scheduler for updating the database

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+const schedule = require('node-schedule');
+const cveProvider = require('./utils/cveProvider');
+const cvePopulator = require('./utils/cvePopulator');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -14,5 +18,10 @@ app.listen(8080, () => {
   console.log('Server started on port 8080...');
 });
 
-// const cveProvider = require('./utils/cveProvider');
-// cveProvider.getCveFiles();
+schedule.scheduleJob('0 0 * * *', () => {
+  cveProvider.getCveFiles();
+});
+
+schedule.scheduleJob('5 0 * * *', () => {
+  cvePopulator.updateDatabase();
+});
