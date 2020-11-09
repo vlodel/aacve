9ad5ca4cc359ac7a492b46a14eefcb354e01483b
Vlad Apostol <vladdapostol@gmail.com>
2020-11-09 13:17:13 +0200

    started changing and refactoring the whole front-end

--- frontend/aacve/src/App.js
+++ frontend/aacve/src/App.js
@@ -1,120 +1,110 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import Login from './Components/Auth/Login';
-import Dashboard from './Components/Dashboard';
-import Register from './Components/Auth/Register';
-import PrivateRoute from './PrivateRoute';
-import { AuthContext } from './AppContext/auth';
-import { AnalyzerContext } from './AppContext/analyzerResults';
+import { Route, Redirect, Switch, HashRouter } from 'react-router-dom';
 import axios from 'axios';
-import Statistics from './Components/Statistics';
-import {
-  MuiThemeProvider,
-  createMuiTheme,
-  CssBaseline,
-} from '@material-ui/core';
 
-function App() {
-  useEffect(() => {
-    axios({
-      method: 'POST',
-      url: `${process.env.REACT_APP_API_URL}/verifyToken`,
-      headers: {
-        Accept: 'application/json',
-        authorization: `Bearer ${authTokens}`,
-      },
-    })
-      .then((result) => {
-        if (result.status === 401) {
-          localStorage.removeItem('tokens');
-          localStorage.removeItem('user');
-          setAuthTokens(null);
-          setCurrentUser(null);
-        }
-      })
-      .catch((err) => {
-        if (err.message.includes('401')) {
-          localStorage.removeItem('tokens');
-          localStorage.removeItem('user');
-          setAuthTokens();
-          setCurrentUser();
-        }
-      });
-  }, []);
+//components
+import Layout from './components//Layout/Layout';
 
-  const existingTokens = JSON.parse(localStorage.getItem('tokens'));
-  const [authTokens, setAuthTokens] = useState(existingTokens);
-  const existingUser = JSON.parse(localStorage.getItem('user'));
-  const [currentUser, setCurrentUser] = useState(existingUser);
-  const [analyzerResults, setAnalyzerResults] = useState(null);
+//context
+import { useUserState } from './context/UserContext';
 
-  const setTokens = (data) => {
-    localStorage.setItem('tokens', JSON.stringify(data));
-    setAuthTokens(data);
-  };
+//pages
+import Login from './pages/login/Login';
+import Error from './pages/error/Error';
 
-  const setUser = (user) => {
-    localStorage.setItem('user', JSON.stringify(user));
-    setCurrentUser(user);
-  };
+export default function App() {
+  // useEffect(() => {
+  //   axios({
+  //     method: 'POST',
+  //     url: `${process.env.REACT_APP_API_URL}/verifyToken`,
+  //     headers: {
+  //       Accept: 'application/json',
+  //       authorization: `Bearer ${authTokens}`,
+  //     },
+  //   })
+  //     .then((result) => {
+  //       if (result.status === 401) {
+  //         localStorage.removeItem('tokens');
+  //         localStorage.removeItem('user');
+  //         setAuthTokens(null);
+  //         setCurrentUser(null);
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       if (err.message.includes('401')) {
+  //         localStorage.removeItem('tokens');
+  //         localStorage.removeItem('user');
+  //         setAuthTokens();
+  //         setCurrentUser();
+  //       }
+  //     });
+  // }, []);
 
-  const setResults = (results) => {
-    setAnalyzerResults(results);
-  };
+  // const existingTokens = JSON.parse(localStorage.getItem('tokens'));
+  // const [authTokens, setAuthTokens] = useState(existingTokens);
+  // const existingUser = JSON.parse(localStorage.getItem('user'));
+  // const [currentUser, setCurrentUser] = useState(existingUser);
+  // const [analyzerResults, setAnalyzerResults] = useState(null);
 
-  const theme = createMuiTheme({
-    palette: {
-      background: {
-        default: '#2e2e2e',
-      },
-      primary: {
-        main: '#c9a95e',
-      },
-      secondary: {
-        main: '#ffffff',
-      },
-    },
-    overrides: {
-      MuiPaginationItem: {
-        root: {
-          color: '#ffffff',
-        },
-      },
-      MuiListItem: {
-        root: {
-          color: '#ffffff',
-        },
-      },
-    },
-  });
+  // const setTokens = (data) => {
+  //   localStorage.setItem('tokens', JSON.stringify(data));
+  //   setAuthTokens(data);
+  // };
+
+  // const setUser = (user) => {
+  //   localStorage.setItem('user', JSON.stringify(user));
+  //   setCurrentUser(user);
+  // };
+
+  // const setResults = (results) => {
+  //   setAnalyzerResults(results);
+  // };
+
+  const { isAuthenticated } = useUserState();
 
   return (
-    <AuthContext.Provider
-      value={{
-        authTokens,
-        setAuthTokens: setTokens,
-        currentUser,
-        setCurrentUser: setUser,
-      }}
-    >
-      <AnalyzerContext.Provider
-        value={{
-          analyzerResults,
-          setAnalyzerResults: setResults,
-        }}
-      >
-        <Router>
-          <div>
-            <Redirect to="/dashboard" />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <PrivateRoute path="/dashboard" component={Dashboard} />
-            <PrivateRoute path="/statistics" component={Statistics} />
-          </div>
-        </Router>
-      </AnalyzerContext.Provider>
-    </AuthContext.Provider>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/app/dashboard" />} />
+        <Route exact path="/app" render={() => <Redirect to="/app/dashboard" />} />
+        <PrivateRoute path="/app" component={Layout} />
+        <PublicRoute path="/login" component={Login} />
+        <Route component={Error} />
+      </Switch>
+    </HashRouter>
   );
-}
 
-export default App;
+  function PrivateRoute({ component, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={(props) =>
+          isAuthenticated ? (
+            React.createElement(component, props)
+          ) : (
+            <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+          )
+        }
+      />
+    );
+  }
+
+  function PublicRoute({ component, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={(props) =>
+          isAuthenticated ? (
+            <Redirect
+              to={{
+                pathname: '/',
+              }}
+            />
+          ) : (
+            React.createElement(component, props)
+          )
+        }
+      />
+    );
+  }
+}
--- frontend/aacve/src/AppContext/analyzerResults.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const AnalyzerContext = createContext();
-
-export function useAnalyzerContext() {
-  return useContext(AnalyzerContext);
-}
